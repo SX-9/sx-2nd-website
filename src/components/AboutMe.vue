c893029f365e32d3d655a88df4b2c4adee3fac9f
SX Spy Agent <sx-91@outlook.com>
2023-05-02 22:21:28 +0700

    update about

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -4,7 +4,7 @@ export default {
   data() {
     return {
       screenWidth: ref(window.innerWidth),
-      description: 'I\'m Satria, a web developer from Indonesia. I started programming in 2021 and initially wanted to learn Python scripting through a tutorial series on YouTube. However, I quit because the series was taking too long to finish. Later, I made my own Discord server and became interested in making Discord bots. Since Discord API updates frequently, it became challenging to learn the new features, so I switched to web development. I started with HTML, CSS, and JavaScript, and now I consider myself an intermediate web developer.',
+      description: "Hi, I'm Satria, a self-taught web developer from Indonesia. I've been coding since 2021 and have experience with Node.js, web development, and Linux. As a student, I love spending my free time coding and experimenting with new technologies. My interests include machine learning, low-level systems, and circuitry. I'm an introvert by nature and prefer to spend most of my time working on personal projects. Thanks for taking the time to learn a little more about me!",
     }
   }
 }
